56e5a560bd32cbc8834b8478980be7e420644e77
Alnagdy <nagdista@gmail.com>
2026-05-09 17:37:08 +0300

    added order model

---
+++ backend/models/Order.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+// !START BUILDING
+const OrderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        qty: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+      },
+    ],
+    shippingAddress: {
+      country: { type: String, required: true },
+      city: { type: String, required: true },
+      address: { type: String, required: true },
+      postalCode: { type: String, required: true },
+    },
+    isPaid: { type: Boolean, required: true, default: false },
+    paymentMethod: { type: String, required: true },
+    paymentResult: {
+      id: { type: String, required: true },
+      status: { type: String },
+      updated_time: { type: String },
+      email_address: { type: String },
+    },
+    paidAt: { type: Date },
+    itemsPrice: { type: Number, required: true, default: 0.0 },
+    taxPrice: { type: Number, required: true, default: 0.0 },
+    shippingPrice: { type: Number, required: true, default: 0.0 },
+    totalPrice: { type: Number, required: true, default: 0.0 },
+    isDelivered: { type: Boolean, required: true, default: false },
+    deliveredAt: { type: Date },
+  },
+
+  { timestamps: true },
+);
+//  !MODEL
+const Order = mongoose.model("Order", OrderSchema);
+// !EXPORT
+export default Order;
